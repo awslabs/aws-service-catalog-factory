511291b91ca3d2de1003ae502c50008417b566e4
Eamonn Faherty <eamonnf@amazon.co.uk>
2019-06-06 18:07:42 +0100

    issue #53

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("servicecatalog_factory/requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="aws-service-catalog-factory",
-    version="0.1.38",
+    version="0.1.39",
     author="Eamonn Faherty",
     author_email="aws-service-catalog-tools@amazon.com",
     description="Making it easier to build out ServiceCatalog products",
