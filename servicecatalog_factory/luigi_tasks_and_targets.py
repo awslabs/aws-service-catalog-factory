aa46be8bac13f9fcb7c312c5b3de7ab47c0cd50f
Eamonn Faherty <eamonnf@amazon.co.uk>
2019-07-02 23:08:32 +0100

    adding last part

--- servicecatalog_factory/luigi_tasks_and_targets.py
+++ servicecatalog_factory/luigi_tasks_and_targets.py
@@ -186,14 +186,34 @@ class EnsureProductVersionDetailsCorrect(luigi.Task):
         )
 
     def requires(self):
-        return {
-            'create_product_task': CreateProductTask(
-                **self.product_args
-            )
-        }
+        return CreateProductTask(
+            **self.product_args
+        )
 
     def run(self):
-        pass
+        product = json.loads(self.input().open('r').read())
+        product_id = product.get('ProductId')
+        version_name = self.version.get('Name')
+        version_active = self.version.get('Active', True)
+
+        with betterboto_client.ClientContextManager('servicecatalog', region_name=self.region) as service_catalog:
+            response = service_catalog.list_provisioning_artifacts(
+                ProductId=product_id
+            )
+            logger.info("Checking through: {}".format(response))
+            for provisioning_artifact_detail in response.get('ProvisioningArtifactDetails', []):
+                if provisioning_artifact_detail.get('Name') == version_name:
+                    logger.info(f"Found matching: {version_name}: {provisioning_artifact_detail}")
+                    if provisioning_artifact_detail.get('Active') != version_active:
+                        logger.info(f"Active status needs to change for: {product.get('Name')} {version_name}")
+                        service_catalog.update_provisioning_artifact(
+                            ProductId=product_id,
+                            ProvisioningArtifactId=provisioning_artifact_detail.get('Id'),
+                            Active=version_active,
+                        )
+
+        with self.output().open('w') as f:
+            f.write("{}")
 
 
 class CreateVersionPipelineTemplateTask(luigi.Task):
