66761e4407cb9228f89b6d69a6005c4cd6f517ed
Eamonn Faherty <eamonnf@amazon.co.uk>
2019-08-09 22:47:47 +0100

    adding associations

--- servicecatalog_factory/luigi_tasks_and_targets.py
+++ servicecatalog_factory/luigi_tasks_and_targets.py
@@ -37,7 +37,6 @@ class CreatePortfolioTask(FactoryTask):
     display_name = luigi.Parameter()
     description = luigi.Parameter(significant=False)
     provider_name = luigi.Parameter(significant=False)
-    associations = luigi.ListParameter(significant=False, default=[])
     tags = luigi.ListParameter(default=[], significant=False)
 
     def params_for_results_display(self):
@@ -85,6 +84,73 @@ class CreatePortfolioTask(FactoryTask):
                     default=str,
                 )
             )
+
+
+class CreatePortfolioAssociationTask(FactoryTask):
+    region = luigi.Parameter()
+    portfolio_group_name = luigi.Parameter()
+    display_name = luigi.Parameter()
+    description = luigi.Parameter(significant=False)
+    provider_name = luigi.Parameter(significant=False)
+    tags = luigi.ListParameter(default=[], significant=False)
+
+    associations = luigi.ListParameter(significant=False, default=[])
+
+    def requires(self):
+        return CreatePortfolioTask(
+            self.region,
+            self.portfolio_group_name,
+            self.display_name,
+            self.description,
+            self.provider_name,
+            self.tags,
+        )
+
+    def params_for_results_display(self):
+        return {
+            "region": self.region,
+            "portfolio_group_name": self.portfolio_group_name,
+            "display_name": self.display_name,
+        }
+
+    def output(self):
+        output_file = f"output/CreatePortfolioAssociationTask/{self.region}-{self.portfolio_group_name}-{self.display_name}.json"
+        return luigi.LocalTarget(output_file)
+
+    def run(self):
+        logger_prefix = f"{self.region}-{self.portfolio_group_name}-{self.display_name}"
+        # with betterboto_client.ClientContextManager(
+        #         'servicecatalog', region_name=self.region
+        # ) as service_catalog:
+        #     generated_portfolio_name = f"{self.portfolio_group_name}-{self.display_name}"
+        #     tags = []
+        #     for t in self.tags:
+        #         tags.append({
+        #             "Key": t.get('Key'),
+        #             "Value": t.get('Value'),
+        #         })
+        #     tags.append({"Key": "ServiceCatalogFactory:Actor", "Value": "Portfolio"})
+        #
+        #     portfolio_detail = aws.get_or_create_portfolio(
+        #         self.description,
+        #         self.provider_name,
+        #         generated_portfolio_name,
+        #         tags,
+        #         service_catalog
+        #     )
+        #
+        # if portfolio_detail is None:
+        #     raise Exception("portfolio_detail was not found or created")
+        #
+        # with self.output().open('w') as f:
+        #     logger.info(f"{logger_prefix}: about to write! {portfolio_detail}")
+        #     f.write(
+        #         json.dumps(
+        #             portfolio_detail,
+        #             indent=4,
+        #             default=str,
+        #         )
+        #     )
 
 
 class CreateProductTask(FactoryTask):
