312e7c62449493de3c93c7254bce82f7e465b2f5
Eamonn Faherty <eamonnf@amazon.co.uk>
2019-08-21 10:48:07 +0100

    now reading AWS_REGION ahead of AWS_DEFAULT_REGION

--- servicecatalog_factory/constants.py
+++ servicecatalog_factory/constants.py
@@ -30,7 +30,6 @@ TERRAFORM_TEMPLATE = 'terraform.template.yaml.j2'
 COMPONENT = 'component.j2'
 COMPONENT_GROUP = 'component_group.j2'
 ASSOCIATIONS = 'associations.j2'
-HOME_REGION = os.environ.get('AWS_DEFAULT_REGION', 'eu-west-1')
-
+HOME_REGION = os.environ.get('AWS_REGION', os.environ.get('AWS_DEFAULT_REGION', 'eu-west-1'))
 
 RESULTS_DIRECTORY = "results"
