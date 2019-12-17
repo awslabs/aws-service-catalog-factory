7c5eb71041c33627d9c912679e0ae1366a031131
Eamonn Faherty <eamonnf@amazon.co.uk>
2019-12-17 12:00:55 +0000

    adding deps

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/awslabs/aws-service-catalog-factory",
     packages=find_packages(),
-    package_data={'servicecatalog_factory': ['*','*/*','*/*/*']},
+    package_data={"servicecatalog_factory": ["*", "*/*", "*/*/*"]},
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
@@ -26,8 +26,34 @@ setup(
         "Natural Language :: English",
     ],
     entry_points={
-        'console_scripts': [
-            'servicecatalog-factory = servicecatalog_factory.cli:cli'
-        ]},
-    install_requires=[],
+        "console_scripts": ["servicecatalog-factory = servicecatalog_factory.cli:cli"]
+    },
+    install_requires=[
+        "better-boto==0.23.0",
+        "boto3==1.10.35",
+        "botocore==1.13.35",
+        "certifi==2019.11.28",
+        "cfn-flip==1.2.1",
+        "chardet==3.0.4",
+        "click==7.0",
+        "colorclass==2.2.0",
+        "docopt==0.6.2",
+        "docutils==0.14",
+        "idna==2.8",
+        "jinja2==2.10.1",
+        "jmespath==0.9.4",
+        "lockfile==0.12.2",
+        "luigi==2.8.6",
+        "markupsafe==1.1.1",
+        "pykwalify==1.7.0",
+        "python-daemon==2.1.2",
+        "python-dateutil==2.8.0; python_version >= '2.7'",
+        "pyyaml==5.1",
+        "requests==2.22.0",
+        "s3transfer==0.2.1",
+        "six==1.13.0",
+        "terminaltables==3.1.0",
+        "tornado==4.5.3",
+        "urllib3==1.25.7; python_version >= '3.4'",
+    ],
 )
