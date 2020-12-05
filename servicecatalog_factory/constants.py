04437522d2a2dfc33a71238f22a54f0b13d88155
Eamonn Faherty <eamonnf@amazon.co.uk>
2020-12-05 23:19:05 +0000

    increasing build timeout (issue #166) and fixing failure to package templates in non default region (issue #167)

--- servicecatalog_factory/constants.py
+++ servicecatalog_factory/constants.py
@@ -66,7 +66,8 @@ PACKAGE_BUILD_SPEC_DEFAULT = """
         build:
           commands:
           {% for region in ALL_REGIONS %}
-            - aws cloudformation package --template $(pwd)/product.template.yaml --s3-bucket sc-factory-artifacts-${ACCOUNT_ID}-{{ region }} --s3-prefix ${STACK_NAME} --output-template-file product.template-{{ region }}.yaml
+            - aws cloudformation package --region {{ region }} --template $(pwd)/product.template.yaml --s3-bucket sc-factory-artifacts-${ACCOUNT_ID}-{{ region }} --s3-prefix ${STACK_NAME} --output-template-file product.template-{{ region }}.yaml
+
           {% endfor %}
       artifacts:
         files:
