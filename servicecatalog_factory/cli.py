56942dc22c6cfb0d26a01f9fb5e1eedf8cf53608
Eamonn Faherty <eamonnf@amazon.co.uk>
2022-03-25 10:41:06 +0000

    chore: cleaning up

--- servicecatalog_factory/cli.py
+++ servicecatalog_factory/cli.py
@@ -513,9 +513,8 @@ def generate_template(
 @click.argument("pipeline-region")
 @click.argument("codepipeline-id")
 @click.argument("region")
-@click.argument("source_path")
 @click.option(
-    "--source_path", default="./", envvar="SOURCE_PATH"
+    "--source_path", default=".", envvar="SOURCE_PATH"
 )
 def create_or_update_provisioning_artifact_from_codepipeline_id(
     pipeline_name, pipeline_region, codepipeline_id, region, source_path
