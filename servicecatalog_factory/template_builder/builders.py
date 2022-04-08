ebde5027b6486746f1f3378abb5902ade0274d67
TRAMPIC Mladen (R-IT) <MLADEN.TRAMPIC@R-IT.AT>
2022-04-08 17:19:40 +0200

    This fixes awslabs/aws-service-catalog-factory#248
    Due to some recent changes Custom source seems not to pass Validation
    stage. Additionally adding option to specify via Build
    BuildEnvironmentComputeType to BuildProject.

--- servicecatalog_factory/template_builder/builders.py
+++ servicecatalog_factory/template_builder/builders.py
@@ -527,7 +527,9 @@ class StackTemplateBuilder(BaseTemplateBuilder):
                     Artifacts=codebuild.Artifacts(Type="CODEPIPELINE"),
                     TimeoutInMinutes=60,
                     Environment=codebuild.Environment(
-                        ComputeType=constants.ENVIRONMENT_COMPUTE_TYPE_DEFAULT,
+                        ComputeType=stages.get("Build", {}).get(
+                            "BuildEnvironmentComputeType", constants.ENVIRONMENT_COMPUTE_TYPE_DEFAULT
+                        ),
                         Image=stages.get("Build", {}).get(
                             "BuildSpecImage", constants.ENVIRONMENT_IMAGE_DEFAULT
                         ),
