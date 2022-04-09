7d03dcef93739607d1b0fb244cd2cabcfc95448d
Eamonn Faherty <eamonnf@amazon.co.uk>
2022-04-10 00:19:17 +0100

    dev

--- servicecatalog_factory/cloudformation_servicecatalog_deploy_action.py
+++ servicecatalog_factory/cloudformation_servicecatalog_deploy_action.py
@@ -131,55 +131,56 @@ def create_or_update_provisioning_artifact(
 
         difference = DeepDiff(existing_template, new_template, ignore_order=True)
 
-        print(difference)
-
-        response = servicecatalog.create_provisioning_artifact(
-            ProductId=product_id,
-            Parameters={
-                "Name": version_name,
-                "Description": description,
-                "Info": {"LoadTemplateFromURL": template_url},
-                "Type": "CLOUD_FORMATION_TEMPLATE",
-                "DisableTemplateValidation": False,
-            },
-        )
-        new_provisioning_artifact_id = response.get("ProvisioningArtifactDetail").get(
-            "Id"
-        )
-        status = "CREATING"
-        while status == "CREATING":
-            time.sleep(3)
-            status = servicecatalog.describe_provisioning_artifact(
+        if len(difference) == 0:
+            click.echo("There were no changes in the template")
+        else:
+            response = servicecatalog.create_provisioning_artifact(
                 ProductId=product_id,
-                ProvisioningArtifactId=new_provisioning_artifact_id,
-            ).get("Status")
-
-        if status == "FAILED":
-            raise Exception("Creating the provisioning artifact failed")
-
-        click.echo(
-            f"Created: {new_provisioning_artifact_id} in: {region} for: {product_id} {version_name}"
-        )
-
-        click.echo(f"Checking for old versions of: {version_name} to delete")
-        provisioning_artifact_details = servicecatalog.list_provisioning_artifacts_single_page(
-            ProductId=product_id
-        ).get(
-            "ProvisioningArtifactDetails", []
-        )
-        for provisioning_artifact_detail in provisioning_artifact_details:
-            if (
-                provisioning_artifact_detail.get("Name") == version_name
-                and provisioning_artifact_detail.get("Id")
-                != new_provisioning_artifact_id
-            ):
-                existing_provisioning_artifact_id = provisioning_artifact_detail.get(
-                    "Id"
-                )
-                click.echo(
-                    f"Deleting version: {existing_provisioning_artifact_id} of: {version_name}"
-                )
-                servicecatalog.delete_provisioning_artifact(
+                Parameters={
+                    "Name": version_name,
+                    "Description": description,
+                    "Info": {"LoadTemplateFromURL": template_url},
+                    "Type": "CLOUD_FORMATION_TEMPLATE",
+                    "DisableTemplateValidation": False,
+                },
+            )
+            new_provisioning_artifact_id = response.get("ProvisioningArtifactDetail").get(
+                "Id"
+            )
+            status = "CREATING"
+            while status == "CREATING":
+                time.sleep(3)
+                status = servicecatalog.describe_provisioning_artifact(
                     ProductId=product_id,
-                    ProvisioningArtifactId=existing_provisioning_artifact_id,
-                )
+                    ProvisioningArtifactId=new_provisioning_artifact_id,
+                ).get("Status")
+
+            if status == "FAILED":
+                raise Exception("Creating the provisioning artifact failed")
+
+            click.echo(
+                f"Created: {new_provisioning_artifact_id} in: {region} for: {product_id} {version_name}"
+            )
+
+            click.echo(f"Checking for old versions of: {version_name} to delete")
+            provisioning_artifact_details = servicecatalog.list_provisioning_artifacts_single_page(
+                ProductId=product_id
+            ).get(
+                "ProvisioningArtifactDetails", []
+            )
+            for provisioning_artifact_detail in provisioning_artifact_details:
+                if (
+                    provisioning_artifact_detail.get("Name") == version_name
+                    and provisioning_artifact_detail.get("Id")
+                    != new_provisioning_artifact_id
+                ):
+                    existing_provisioning_artifact_id = provisioning_artifact_detail.get(
+                        "Id"
+                    )
+                    click.echo(
+                        f"Deleting version: {existing_provisioning_artifact_id} of: {version_name}"
+                    )
+                    servicecatalog.delete_provisioning_artifact(
+                        ProductId=product_id,
+                        ProvisioningArtifactId=existing_provisioning_artifact_id,
+                    )
