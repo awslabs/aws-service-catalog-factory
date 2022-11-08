a578daa7bcdf12e13ecf5dff8ac68fe611267f89
Eamonn Faherty <eamonnf@amazon.co.uk>
2022-11-08 16:31:19 +0000

    preparing for release

--- setup.py
+++ setup.py
@@ -21,10 +21,7 @@ packages = \
 
 package_data = \
 {'': ['*'],
- 'servicecatalog_factory': ['portfolios/*',
-                            'schema/*',
-                            'templates/*',
-                            'templates/tests/*']}
+ 'servicecatalog_factory': ['portfolios/*', 'schema/*', 'templates/*']}
 
 install_requires = \
 ['PyYAML==5.4',
@@ -49,7 +46,7 @@ entry_points = \
 
 setup_kwargs = {
     'name': 'aws-service-catalog-factory',
-    'version': '0.92.0',
+    'version': '0.93.0',
     'description': 'Making it easier to build ServiceCatalog products',
     'long_description': '# aws-service-catalog-factory\n\n![logo](./docs/logo.png) \n\n## What is it?\nThis is a python3 framework that makes it easier to build multi region AWS Service Catalog portfolios.\n\nWith this framework you define a portfolio in YAML.  For each product version in your portfolio you specify which git \nrepository it is in and the framework will build out AWS CodePipelines for each product version.\n\nThese CodePipelines can run CFN_NAG and Cloudformation_rspec on your templates enabling you to check your templates are \ngood quality that they are functionally correct.\n\n## Getting started\n\nYou can read the [installation how to](https://service-catalog-tools-workshop.com/30-how-tos/10-installation/20-service-catalog-factory.html)\nor you can read through the [every day use](https://service-catalog-tools-workshop.com/30-how-tos/50-every-day-use.html)\nguides.\n\nYou can read the [documentation](https://aws-service-catalog-factory.readthedocs.io/en/latest/) to understand the inner \nworkings. \n\n\n## Going further\n\nThe framework is one of a pair.  The other is [aws-service-catalog-puppet](https://github.com/awslabs/aws-service-catalog-puppet).\nWith Service Catalog Puppet you can provision products into multiple regions of multiple accounts using YAML and you can \nshare portfolios across multiple regions of multiple accounts. \n\n## License\n\nThis library is licensed under the Apache 2.0 License. \n',
     'author': 'Eamonn Faherty',
